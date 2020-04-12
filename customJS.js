9ed1f2f70d86d4407df0f1d6c78ce14d80155bd6
PrimeXenon <zakiyfahmi@gmail.com>
2020-04-12 12:17:36 +0700

    C-Net Exam

---
+++ customJS.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#slider1").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+     });
+});
